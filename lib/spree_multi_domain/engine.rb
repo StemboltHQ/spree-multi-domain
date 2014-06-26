37899d6452fb6a474a17a80cc8ddbfcec13aa1d2
Adam Mueller <adam@freerunningtech.com>
2014-06-26 16:40:17 -0700

    Move order module_eval to decorator.
    Otherwise code reloading will break everything. >.<

--- lib/spree_multi_domain/engine.rb
+++ lib/spree_multi_domain/engine.rb
@@ -45,34 +45,6 @@ module SpreeMultiDomain
       end
     end
 
-    initializer "current order decoration" do |app|
-      require 'spree/core/controller_helpers/order'
-      ::Spree::Core::ControllerHelpers::Order.module_eval do
-        def current_order_with_multi_domain(options = {})
-          options[:create_order_if_necessary] ||= false
-          current_order_without_multi_domain(options)
-
-          if @current_order && current_store && @current_order.store.nil?
-            @current_order.update_column(:store_id, current_store.id)
-          end
-
-          @current_order
-        end
-        alias_method_chain :current_order, :multi_domain
-
-        def set_current_order
-          if user = try_spree_current_user
-            last_incomplete_order = user.last_incomplete_order_on_store(current_store)
-            if session[:order_id].nil? && last_incomplete_order
-              session[:order_id] = last_incomplete_order.id
-            elsif current_order(create_order_if_necessary: true) && last_incomplete_order && current_order != last_incomplete_order
-              current_order.merge!(last_incomplete_order, user)
-            end
-          end
-        end
-      end
-    end
-
     initializer 'spree.promo.register.promotions.rules' do |app|
       app.config.spree.promotions.rules << Spree::Promotion::Rules::Store
     end
