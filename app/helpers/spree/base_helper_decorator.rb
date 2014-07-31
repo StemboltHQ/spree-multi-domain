da15c30b0efcb5ac88a885165031ef439bd57ac2
Richard Wilson <richard@freerunningtech.com>
2014-07-31 09:25:58 -0700

    Fix base helpers to allow for granularity

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,8 +1,14 @@
 module Spree
   module BaseHelper
     alias_method :old_meta_data, :meta_data
+
     def meta_data
-      old_meta_data.merge(
+      # Ignore blank values
+      data = old_meta_data.delete_if { |k, v| v.blank? }
+
+      # By default, spree allows objects to override the keywords if they're defined.
+      # This restores that functionality while falling back to the stores attributes.
+      data.reverse_merge(
         keywords: current_store.meta_keywords,
         description: current_store.meta_description
       )
