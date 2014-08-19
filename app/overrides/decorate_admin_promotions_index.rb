825b67c36b28de737da8045a4df6de1794e89edb
Richard Wilson <richard@freerunningtech.com>
2014-08-19 12:56:15 -0700

    Add store to the promotion table.

--- app/overrides/decorate_admin_promotions_index.rb
+++ app/overrides/decorate_admin_promotions_index.rb
@@ -11,3 +11,23 @@ Deface::Override.new(
   <div class="clearfix"></div>
   text
 )
+
+Deface::Override.new(
+  virtual_path: "spree/admin/promotions/index",
+  name: "add_store_to_th",
+  insert_before: "th.actions",
+  text: <<-text
+    <th><%= Spree.t(:store) %></th>
+  text
+)
+
+Deface::Override.new(
+  virtual_path: "spree/admin/promotions/index",
+  name: "add_store_to_tr",
+  insert_before: "td.actions",
+  text: <<-text
+    <td class="align-center"><%= promotion.store.try!(:name) %></th>
+  text
+)
+
+
